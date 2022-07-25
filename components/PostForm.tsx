693f2c9864b57ceed4b281482954d56960903b02
artyom <artyom@MacBook-Pro-artyom.local>
2022-07-25 19:26:27 +0300

    create

--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -1,5 +1,7 @@
-import TextField from "@mui/material/TextField";
 import { useState } from "react";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import dynamic from "next/dynamic";
 import styles from "./PostForm.module.css";
 
@@ -17,12 +19,28 @@ export const PostForm: React.FC<PostFormProps> = ({ name }) => {
   const [text, setText] = useState("");
   return (
     <div className={styles.post__form}>
-      <TextField
-        className={styles.post__header}
-        placeholder={name ? name : "Create a post"}
-        onChange={(e) => setText(e.target.value)}
-        value={text}
-      ></TextField>
+      <div className={styles.post__flexbox}>
+        <h2 className={styles.post__title}>
+          Create a <span>post</span>{" "}
+        </h2>
+        <Input
+          classes={{ root: styles.post__header }}
+          placeholder={name ? name : "Title"}
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+          fullWidth
+        ></Input>
+        <Input
+          classes={{
+            root: styles.post__text,
+            focused: styles.post_focused,
+          }}
+          placeholder={name ? name : "Type something..."}
+          fullWidth
+          multiline
+        ></Input>
+        <Button classes={{ root: styles.post__button }}>Publish</Button>
+      </div>
     </div>
   );
 };
